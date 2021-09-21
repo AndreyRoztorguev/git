9cb4886b59c589e0f86e325175a7431cfcf37d71
Andrey Roztorguev <andrew.roztor99@gmail.com>
2021-09-21 15:58:15 +0300

    asd

--- src/file1.js
+++ src/file1.js
@@ -1,2 +1,8 @@
+<<<<<<< HEAD
 'file1.js'
 "hello4"
+=======
+"file1.js";
+"hello3";
+efef;
+>>>>>>> 75ce099 (fix)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 function index() {
   return <div>Hello Master</div>;
 }
+33;
 
 function a() {
   return null;
