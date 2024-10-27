f0d919eb5afe800046252af2a0a51934656adc4b
Andrey <andrew.roztor99@gmail.com>
2024-10-27 16:31:24 +0200

    log(NaN)

--- src/features/sum.js
+++ src/features/sum.js
@@ -1,6 +1,9 @@
 function sum(a, b) {
   let sum = Number(Number(parseInt(a)) + Number(parseInt(b)));
-  if (Number.isNaN(sum)) return 0;
+  if (Number.isNaN(sum)) {
+    console.log("NaN");
+    return 0;
+  }
   return sum;
 }
 
